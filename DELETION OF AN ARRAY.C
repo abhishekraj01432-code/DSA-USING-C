e33f1b82483f6a7fc0fea5a33122d0f417bc07be
ABHISHEK VERMA <abhishekraj01432@gmail.com>
2026-02-18 21:50:01 +0530

    Add files via upload

---
+++ DELETION OF AN ARRAY.C
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int a[100], n, pos, i;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter elements: ");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    printf("Enter position to delete (1 to %d): ", n);
+    scanf("%d", &pos);
+
+    if(pos < 1 || pos > n) {
+        printf("Invalid position!\n");
+    } else
+    {
+        for(i = pos - 1; i < n - 1; i++) {
+            a[i] = a[i + 1];
+        }
+        n--;
+        printf("Array after deletion:\n");
+        for(i = 0; i < n; i++) {
+            printf("%d ", a[i]);
+        }
+    }
+    printf("\nThis is Abhishek Verma succesfully done deletion of an array");
+
+    return 0;
+}
